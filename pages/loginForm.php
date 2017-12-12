39616943423d0a3d428f1e7e35b304e7e2e10eee
nh95 <nh95@njit.edu>
2017-12-12 17:04:29 -0500

    Added login form

---
+++ pages/loginForm.php
@@ -0,0 +1,36 @@
+/**
+ * Created by PhpStorm.
+ * User: navee
+ * Date: 12/12/2017
+ * Time: 1:51 PM
+ */
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+
+<form action="index.php?action=homepage" method="post">
+    User name: <input type="text" name="fname"><br>
+    Password: <input type="password" name="password"><br>
+    <input type="submit" value="Login">
+</form>
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
