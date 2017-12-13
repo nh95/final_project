9cd43ce84c03219ab15d8075321244cd9fb09a4b
naveen basavaraj hottigimath <nh95@afsconnect1.njit.edu>
2017-12-13 00:55:56 -0500

    added form for todo

---
+++ pages/todo.php
@@ -0,0 +1,19 @@
+//form to add and edit todo
+<html>
+<body>
+<form action="index.php?page=task&action=create" method="post">
+    First name: <input type="text" name="fname"><br>
+        Last name: <input type="text" name="lname"><br>
+	    Email: <input type="text" name="email"><br>
+	        Phone: <input type="text" name="phone"><br>
+		    Birthday: <input type="text" name="birthday"><br>
+		        Gender: <input type="text" name="gender"><br>
+			    Password: <input type="password" name="password"><br>
+			        <input type="submit" value="Submit form">
+				</form>
+
+
+				<script src="js/scripts.js"></script>
+				</body>
+				</html>
+
