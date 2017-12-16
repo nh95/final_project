b84f7435dc8c9c807dd5fede727b61eecbd95836
nh95 <nh95@njit.edu>
2017-12-16 17:00:20 -0500

    header & footer inserted

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -1,31 +1,18 @@
-<!doctype html>
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
 <?php
 //this is how you print something
-
-print utility\htmlTable::genarateTableFromMultiArray($data);
-
-
+include 'header.php';
+if($data == false){
+        echo '<p> No Task created!</p>';
+ }else {
+    print utility\htmlTable::genarateTableFromMultiArray($data);
+}
 ?>
-
-
-<script src="js/scripts.js"></script>
+<html>
+<body>
+<form action="index.php?page=tasks&action=create" method="post" id="form1">
+    <button type="submit" form="form1" value="create">Create Task</button>
+    </form>
 </body>
 </html>
+
+
