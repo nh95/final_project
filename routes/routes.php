4ab9e72c59d44a69beae6dc542f8a5b4af16f6b5
nh95 <nh95@njit.edu>
2017-12-16 19:30:02 -0500

    New routes has been added

--- routes/routes.php
+++ routes/routes.php
@@ -154,6 +154,38 @@ class routes
 			        $route->controller = 'tasksController';
 				        $route->method = 'store';
 					        $routes[] = $route;
+        $route = new route();
+                $route->http_method = 'POST';
+                $route->action = 'create';
+                $route->page = 'tasks';
+                $route->controller = 'tasksController';
+                $route->method = 'create';
+                $routes[] = $route;
+        //for edit form
+                $route = new route();
+                $route->http_method = 'GET';
+                $route->action = 'create';
+                $route->page = 'tasks';
+                $route->controller = 'tasksController';
+                $route->method = 'form';
+                $routes[] = $route;
+                //new edit save
+                $route = new route();
+                $route->http_method = 'POST';
+                $route->action = 'save';
+                $route->page = 'tasks';
+                $route->controller = 'tasksController';
+                $route->method = 'save';
+                $routes[] = $route;
+
+        //logout
+                $route = new route();
+                $route->http_method = 'GET';
+                $route->action = 'logout';
+                $route->page = 'accounts';
+                $route->controller = 'accountsController';
+                $route->method = 'logout';
+                $routes[] = $route;
 						
 
         return $routes;
